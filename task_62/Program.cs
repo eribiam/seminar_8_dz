62d39afee75793405abcd6cd3bcec6582d36b4a6
eribiam <boss.vede@mail.ru>
2022-09-15 18:47:36 +0700

    comments_to_tasks

--- task_62/Program.cs
+++ task_62/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//сделать 4 цикла внутри цикла и уменьшать или увеличивать индексв и карждую итерацию уменьшать размер строки или столбца
